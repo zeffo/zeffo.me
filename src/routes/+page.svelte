89f73a5a18e95d40f52d847ca63ae8c7d567d7b0
Aman Phatak <phatakaman@gmail.com>
2023-04-25 12:17:04 +0530

    feat: start work on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,30 @@
 
 <style>
     p {
-        text-align: center;
+        text-align: left;
+        line-height: 20px;
+       
+    }
+
+    .container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
     }
 </style>
 
-<p>
-    Hello there!
-    I ❤️ automation, concurrent programming and Star Wars.
-    You can find me on Discord and GitHub.
-</p>
+<br>
+<div class="container">
+    <p>
+        hello there! i'm aman ~
+    </p>
+    <p>  
+        i like programming (especially automation and concurrency). <br>
+        you can see my projects 
+        <a href="/projects">here</a>. <br><br>
+        i also like rescuing animals. you can see some of them 
+        <a href="/animals">here</a>. (very cute) <br><br>
+        
+    </p>
+</div>
