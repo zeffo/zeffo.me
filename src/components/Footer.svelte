450ca333e1d635fbfddcb17dcb14f370017051f3
Aman Phatak <phatakaman@gmail.com>
2023-04-26 00:33:45 +0530

    feat: complete projects page

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -43,3 +43,4 @@
         {/each}
     </div>
 </footer>
+
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,8 +2,13 @@
 	import Navbar from '../../components/Nav.svelte'
     import Footer from '../../components/Footer.svelte';
 
-
-
+    let cards = [
+        ['hk', 'https://github.com/zeffo/HK', 'a multipurpose discord bot', "--ctp-mocha-red"],
+        ['ambilight', 'https://youtu.be/_VMQRKT320A', 'synchronize monitor colors with an ARGB strip in real time (very pretty)', "--ctp-mocha-peach"],
+        ['zeffo.me', '/', 'this website, made with svelte', "--ctp-mocha-yellow"],
+        ['periodico', 'https://www.youtube.com/watch?v=BfJffhaNUBo', 'robot to help newspaper vendors cut down on delivery times', "--ctp-mocha-green"],
+        ['daksh', 'https://github.com/zeffo/daksh', 'utilities to grab audio from youtube and build music players', "--ctp-mocha-blue"]
+    ]
 
 </script>
 
@@ -21,15 +26,57 @@
         align-items: center;
         justify-content: center;
         flex-direction: column;
+        padding-bottom:100px;
     }
 
+    .cards {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        flex-wrap: wrap;
+        gap: 50px;
+        max-width: 1000px;
+    }
 
+    div > .card {
+        text-decoration: none;
+        background-color: var(--ctp-mocha-base);
+        color: var(--ctp-mocha-text);
+        border: 2px solid;
+        border-radius: 10px;
+        width: 250px;
+        height: 120px;
+        text-align: left;
+        padding: 0 20px 20px;
+    }
+
+    .card:hover {
+        text-shadow: none;
+    }
+
+    .card:hover > h1 {
+        text-shadow: 0 0 5px;
+        text-decoration: underline;
+    }
 
 </style>
 <div class="container">
     <p>
         ~ projects ~      
     </p>
+    <div class="cards">
+        {#each cards as [title, link, desc, color]}
+            <a href={link} target="_blank" rel="noreferrer" class="card" style="border-color: var({color})">
+                <h1 style="color: var({color})">
+                    {title}
+                </h1>
+                <span>
+                    {desc}
+                </span>
+            </a>
+        {/each}
+    </div>
 </div>
 
 <Footer />
