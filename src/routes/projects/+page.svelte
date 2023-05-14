97332385ece0f38bf1dc2d33cc7504612a26bed3
Aman Phatak <phatakaman@gmail.com>
2023-05-14 15:03:35 +0530

    fix: card paddings (firefox)

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,8 +3,8 @@
     import Footer from '../../components/Footer.svelte';
 
     let cards = [
-        ['hk', 'https://github.com/zeffo/HK', 'a multipurpose discord bot themed on star wars', "--ctp-mocha-red"],
         ['ambilight', 'https://youtu.be/_VMQRKT320A', 'synchronize monitor colors with an ARGB strip in real time', "--ctp-mocha-peach"],
+        ['hk', 'https://github.com/zeffo/HK', 'a multipurpose discord bot themed on star wars', "--ctp-mocha-red"],
         ['zeffo.me', 'https://github.com/zeffo/zeffo.me', 'this website, made with svelte and hosted on cf pages', "--ctp-mocha-yellow"],
         ['periodico', 'https://www.youtube.com/watch?v=BfJffhaNUBo', 'robot to help newspaper vendors cut down on delivery times', "--ctp-mocha-green"],
         ['daksh', 'https://github.com/zeffo/daksh', 'utilities to grab audio from youtube and build music players', "--ctp-mocha-blue"]
@@ -46,16 +46,26 @@
         border: 4px solid;
         border-radius: 10px;
         width: 250px;
-        height: 120px;
+        height: 110px;
         text-align: left;
         padding: 0 20px 30px;
+    }
+
+    .card-title {
+        margin-bottom: 10px;
+    }
+
+    .card-body {
+        text-align: left;
+        margin: 0;
+        padding: 0;
     }
 
     .card:hover {
         text-shadow: none;
     }
 
-    .card:hover > h1 {
+    .card:hover > .card-title {
         text-shadow: 0 0 5px;
         text-decoration: underline;
     }
@@ -64,18 +74,18 @@
 <div class="container">
     <p>
         ~ projects ~  <br><br>
-        (you can also check out my 
+        (you can also check out my
         <a href="https://github.com/zeffo">github</a>)
     </p>
     <div class="cards">
         {#each cards as [title, link, desc, color]}
             <a href={link} target="_blank" rel="noreferrer" class="card" style="border-color: var({color})">
-                <h1 style="color: var({color})">
+                <h1 style="color: var({color})" class="card-title">
                     {title}
                 </h1>
-                <span>
+                <p class="card-body">
                     {desc}
-                </span>
+                </p>
             </a>
         {/each}
     </div>
