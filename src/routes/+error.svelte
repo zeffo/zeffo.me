7fe21a49af584ece03c56e7e069cb4110d2fc5e4
Aman Phatak <aman@zeffo.me>
2024-02-11 14:39:20 +0530

    feat: customize error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<style>
+    .contents {
+        align-items: center;
+    }
+
+    .status {
+        font-size: 100px;
+        font-weight: bold;
+        color: var(--red);
+        padding: 0;
+        margin: 0;
+        text-shadow: 0 0 5px;
+    }
+
+    .message {
+        font-size: larger;
+        font-style: italic;
+    }
+
+    .empty {
+        height: 300px;
+    }
+
+</style>
+
+<div class="contents">
+    <h1 class="status"> {$page.status} </h1>
+    <p class="message"> ~ {$page.error.message} ~ </p>
+    <div class="empty"></div>
+</div>
