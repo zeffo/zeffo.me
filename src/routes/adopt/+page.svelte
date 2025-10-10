f4e260e274e52d5726b0f9e4082394631ca5afd2
Aman Phatak <aman@zeffo.me>
2025-10-10 21:59:03 +0530

    feat: add lego adoption page

---
+++ src/routes/adopt/+page.svelte
@@ -0,0 +1,72 @@
+<body>
+  <div id="Wrapper" class="container">
+  <div id="Contents" class="container column">
+    <h1>
+      ~ adopting a pet
+    </h1>
+    <p>
+    Adopting a pet is a significant and rewarding decision that requires careful planning and consideration.<br>
+    As a pet parent, you will be financially and physically responsible for:
+      <ol>
+        <li> Managing your pet's healthcare: vaccinations, sterilization, check-ups, surgeries </li>
+        <li> Providing a healthy diet </li>
+        <li> General wellbeing and safety: walks, grooming, playtime </li>
+      </ol>
+    You can contact us on whatsapp:
+    <div class="container row" id="Contacts">
+      <a href="https://wa.me/918408927603" id="Alka">Alka</a>
+      <a href="https://wa.me/918390030197" id="Aman">Aman</a>
+    </div>
+  </div>
+  </div>
+</body>
+
+<style>
+
+#Contents {
+  justify-content: center;
+  padding: 20px;
+}
+
+h1 {
+  font-size: 18pt;
+  color: var(--yellow);
+}
+
+p {
+  max-width: 1000px;
+  text-align: left; 
+  gap: 10px;
+}
+
+#Wrapper {
+  justify-content: center;
+  align-items: center;
+  padding: 0 20px;
+}
+
+div > a {
+  color: var(--crust);
+  border: 1px solid var(--crust);
+  border-radius: 5px;
+  padding: 2px 5px;
+  width: 100px;
+  text-align: center;
+  text-decoration: none;
+}
+
+#Alka {
+  background-color: var(--teal);
+}
+
+#Aman {
+  background-color: var(--green);
+}
+
+#Contacts {
+  gap: 10px;
+  padding: 20px 0;
+}
+
+
+</style>
---
+++ src/routes/adopt/lego/+page.svelte
@@ -0,0 +1,62 @@
+
+<body>
+  <div class="container column" id="Contents">
+    <h1> Lego </h1>
+    <p>
+      A month old and weighing around a quarter kilogram, Lego is tiny yet fiercely brave soul looking for a family and home.
+    </p>
+    <a href="/adopt" id="Adopt">
+      adopt me!
+    </a>
+    <div class="container row" id="Images">
+      <img src="/adopt/lego/lego_basket.jpg" alt="lego sleeping on my desk" />
+      <img src="/adopt/lego/lego_printer.jpg" alt="lego dozing off on a heated 3d printer bed" />
+      <img src="/adopt/lego/lego_doze.jpg" alt="lego taking a nap during a photoshoot" />
+    </div>
+  </div>
+</body>
+
+<style>
+
+p {
+  text-align: center;
+  margin: 10px;
+}
+
+#Adopt {
+  border-radius: 5px;
+  border: 1px solid var(--crust);
+  background: linear-gradient(var(--pink), var(--red), var(--peach));
+  color: var(--crust);
+  margin-bottom: 10px;
+  text-decoration: none;
+  padding: 2px 5px;
+}
+
+#Contents {
+  justify-content: center;
+  align-items: center;
+}
+
+#Images {
+  flex-wrap: wrap;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+}
+ 
+#Images > img {
+  border-radius: 5px;
+  border: 1px solid var(--crust);
+  height: 300px;
+}
+
+h1 {
+  color: var(--peach);
+  font-size: 18pt;
+  margin-bottom: 0;
+}
+
+</style>
+
+
