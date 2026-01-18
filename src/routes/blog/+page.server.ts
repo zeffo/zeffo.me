4b1ab07ee9ba06eea2f59e54ba2a9900e295bd32
Aman Phatak <aman@zeffo.me>
2026-01-18 11:30:30 +0530

    feat: remove unnecessary api

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,7 +1,32 @@
 import type { Post } from "$lib/types";
 
-export async function load({ fetch }) {
-  const response = await fetch("api/blog");
-  const posts: Post[] = await response.json();
+async function getPosts() {
+  let posts: Post[] = [];
+
+  const paths = import.meta.glob("/src/routes/blog/*/*.md", {
+    eager: true,
+  });
+
+  for (const path in paths) {
+    const file = paths[path];
+    const slug = path.split("/").at(-2)?.replace(".md", "");
+
+    if (file && typeof file === "object" && "metadata" in file && slug) {
+      const metadata = file.metadata as Omit<Post, "slug">;
+      const post = { ...metadata, slug } satisfies Post;
+      post.published && posts.push(post);
+    }
+  }
+
+  posts = posts.sort(
+    (first, second) =>
+      new Date(second.date).getTime() - new Date(first.date).getTime(),
+  );
+
+  return posts;
+}
+
+export async function load() {
+  const posts = await getPosts();
   return { posts };
 }
