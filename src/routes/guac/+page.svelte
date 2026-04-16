4a1b447126f02dac043fb101a28d5bfe073afe9c
Aman Phatak <aman@zeffo.me>
2026-04-16 20:00:51 +0530

    feat: update guac link

--- src/routes/guac/+page.svelte
+++ src/routes/guac/+page.svelte
@@ -1,10 +1,41 @@
 <head>
     <meta property="og:title" content="Guacamolecule" />
     <meta property="og:type" content="website" />
-    <meta property="og:description" content="Aman's discord server" />
+    <meta property="og:description" content="THE discord server" />
     <meta property="og:site_name" content="Guacamolecule">
     <meta name="theme-color" content="#f5c2e7">
-    <meta http-equiv="refresh" content="0; url=https://discord.gg/6frntWtKFj" />
+    <meta http-equiv="refresh" content="0; url=https://discord.gg/KaCE35AaU5" />
 </head>
+
+<style>
+    .contents {
+        align-items: center;
+    }
+
+    .status {
+        font-size: 100px;
+        font-weight: bold;
+        color: var(--red);
+        padding: 0;
+        margin: 0;
+        text-shadow: 0 0 5px;
+    }
+
+    .message {
+        font-size: larger;
+        font-style: italic;
+    }
+
+    .empty {
+        height: 300px;
+    }
+
+</style>
+
+<div class="contents">
+    <h1 class="status"> redirecting </h1>
+    <p class="message"> to guacamolecule </p>
+    <div class="empty"></div>
+</div>
 
  
