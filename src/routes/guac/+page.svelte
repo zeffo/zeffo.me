4e6daca9fdf1fa8706596beed673873ab8288101
Aman Phatak <aman@zeffo.me>
2026-05-07 19:34:21 +0530

    feat: add pixelart guac logo by @friedchaawal to invite

--- src/routes/guac/+page.svelte
+++ src/routes/guac/+page.svelte
@@ -1,19 +1,20 @@
 <head>
     <meta property="og:title" content="Guacamolecule" />
     <meta property="og:type" content="website" />
-    <meta property="og:description" content="THE discord server" />
+    <meta property="og:description" content="rainbows and fairies" />
     <meta property="og:site_name" content="Guacamolecule">
+    <meta property="og:image" content="https://zeffo.me/guac-logo.png" />
     <meta name="theme-color" content="#f5c2e7">
-    <meta http-equiv="refresh" content="0; url=https://discord.gg/KaCE35AaU5" />
+    <meta http-equiv="refresh" content="0;url=https://discord.gg/KaCE35AaU5" />
 </head>
 
 <style>
     .contents {
-        align-items: center;
+        text-align: center;
     }
 
     .status {
-        font-size: 100px;
+        font-size: 40pt;
         font-weight: bold;
         color: var(--red);
         padding: 0;
@@ -26,16 +27,21 @@
         font-style: italic;
     }
 
+    img {
+      height: 50px;
+      width: 50px;
+    }
+
     .empty {
         height: 300px;
     }
-
 </style>
 
 <div class="contents">
-    <h1 class="status"> redirecting </h1>
-    <p class="message"> to guacamolecule </p>
-    <div class="empty"></div>
+  <h1 class="status"> redirecting </h1>
+  <p class="message"> to guacamolecule </p>
+  <img src="guac-logo.png">
+  <div class="empty"></div>
 </div>
 
  
